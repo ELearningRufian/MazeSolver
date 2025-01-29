086125a26f4592fccdb8d9897394d0e673b1e7e3
erufian <github@rufian.zilbermann.com>
2025-01-29 14:21:28 -0500

    L9 Breaking Down Walls in the Maze. Update Cell class for testability

--- src/cell.py
+++ src/cell.py
@@ -8,20 +8,23 @@ class Cell:
         self.has_top_wall = True
         self.has_bottom_wall = True
         self._win = win
+        self._x0 = None
+        self._y0 = None
+        self._x1 = None
+        self._y1 = None
 
     def draw(self, p0, p1, cell_color):
-        self._x0 = p0.x
-        self._y0 = p0.y
-        self._x1 = p1.x
-        self._y1 = p1.y
-        if self.has_left_wall:
-            Line(Point(self._x0, self._y0), Point(self._x0, self._y1)).draw(self._win.canvas, cell_color)
-        if self.has_top_wall:
-            Line(Point(self._x1, self._y0), Point(self._x0, self._y0)).draw(self._win.canvas, cell_color)
-        if self.has_right_wall:
-            Line(Point(self._x1, self._y1), Point(self._x1, self._y0)).draw(self._win.canvas, cell_color)
-        if self.has_bottom_wall:
-            Line(Point(self._x0, self._y1), Point(self._x1, self._y1)).draw(self._win.canvas, cell_color)
+        if p0 is not None:
+            self._x0 = p0.x
+            self._y0 = p0.y
+        if p1 is not None:
+            self._x1 = p1.x
+            self._y1 = p1.y
+        if self._x0 is not None and self._y0 is not None and self._x1 is not None and self._y1 is not None:
+            Line(Point(self._x0, self._y0), Point(self._x0, self._y1)).draw(self._win.canvas, cell_color if self.has_left_wall else "#d9d9d9")
+            Line(Point(self._x1, self._y0), Point(self._x0, self._y0)).draw(self._win.canvas, cell_color if self.has_top_wall else "#d9d9d9")
+            Line(Point(self._x1, self._y1), Point(self._x1, self._y0)).draw(self._win.canvas, cell_color if self.has_right_wall else "#d9d9d9")
+            Line(Point(self._x0, self._y1), Point(self._x1, self._y1)).draw(self._win.canvas, cell_color if self.has_bottom_wall else "#d9d9d9")
 
     def draw_move(self, to_cell, undo=False):
         from_point = Point((self._x0 + self._x1)/2, (self._y0 + self._y1)/2)
--- src/maze.py
+++ src/maze.py
@@ -14,7 +14,9 @@ class Maze:
         self.cell_size_x = cell_size_x
         self.cell_size_y = cell_size_y
         self.win = win
+        self.cell_color = "black"
         self._create_cells()
+        self._break_entrance_and_exit()
 
     def _create_cells(self):
         self._cells = [[Cell(self.win) for r in range(self.num_rows)] for c in range(self.num_cols)]
@@ -25,9 +27,17 @@ class Maze:
             return
         p0 = Point(self.x1 + i * self.cell_size_x, self.y1 + j * self.cell_size_y)
         p1 = Point(self.x1 + (i + 1) * self.cell_size_x, self.y1 + (j + 1) * self.cell_size_y)
-        self._cells[i][j].draw(p0, p1, "black")
+        self._cells[i][j].draw(p0, p1, self.cell_color)
         self._animate()
 
     def _animate(self):
         self.win.redraw()
         sleep(0.05)
+
+    def _break_entrance_and_exit(self):
+        entrance_cell = self._cells[0][0]
+        exit_cell = self._cells[-1][-1]
+        entrance_cell.has_top_wall = False
+        exit_cell.has_bottom_wall = False
+        entrance_cell.draw(None, None, self.cell_color)
+        exit_cell.draw(None, None, self.cell_color)
