c29d9e73fd8b1eb88d78ae54aab3548b34d5f3d2
erufian <github@rufian.zilbermann.com>
2025-01-29 13:13:37 -0500

    L8 Maze tests, add exceptions in Maze __init__ and fix row/cols handling

--- src/maze.py
+++ src/maze.py
@@ -2,7 +2,11 @@ from time import sleep
 from cell import *
 
 class Maze:
-    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win = None):
+        if 1 > num_rows:
+            raise ValueError("The maze must have at least 1 row")
+        if 1 > num_cols:
+            raise ValueError("The maze must have at least 1 column")
         self.x1 = x1
         self.y1 = y1
         self.num_rows = num_rows
@@ -13,12 +17,14 @@ class Maze:
         self._create_cells()
 
     def _create_cells(self):
-        self._cells = [[Cell(self.win) for c in range(self.num_cols)] for r in range(self.num_rows)]
-        [self._draw_cell(i, j) for i in range(self.num_rows) for j in range(self.num_cols)]
+        self._cells = [[Cell(self.win) for r in range(self.num_rows)] for c in range(self.num_cols)]
+        [self._draw_cell(i, j) for j in range(self.num_rows) for i in range(self.num_cols)]
 
     def _draw_cell(self, i, j):
-        p0 = Point(self.x1 + j * self.cell_size_x, self.y1 + i * self.cell_size_y)
-        p1 = Point(self.x1 + (j + 1) * self.cell_size_x, self.y1 + (i + 1) * self.cell_size_y)
+        if self.win is None:
+            return
+        p0 = Point(self.x1 + i * self.cell_size_x, self.y1 + j * self.cell_size_y)
+        p1 = Point(self.x1 + (i + 1) * self.cell_size_x, self.y1 + (j + 1) * self.cell_size_y)
         self._cells[i][j].draw(p0, p1, "black")
         self._animate()
 
---
+++ src/tests.py
@@ -0,0 +1,51 @@
+import unittest
+from maze import Maze
+
+class Tests(unittest.TestCase):
+    def test_maze_create_cells(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+
+    def test_maze_create_cells_negative_rows(self):
+        num_cols = 12
+        num_rows = -10
+        with self.assertRaises(ValueError) as context:
+            m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        result = str(context.exception)
+        expected = "The maze must have at least 1 row"
+        self.assertEqual(result, expected)
+
+    def test_maze_create_cells_negative_columns(self):
+        num_cols = -12
+        num_rows = 10
+        with self.assertRaises(ValueError) as context:
+            m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        result = str(context.exception)
+        expected = "The maze must have at least 1 column"
+        self.assertEqual(result, expected)
+
+    # Negative coordinates could go off-canvas but should not raise exceptions
+    def test_maze_create_cells_negative_coordinates(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(-100, -100, num_rows, num_cols, -100, -100) 
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+
+if __name__ == "__main__":
+    unittest.main()
